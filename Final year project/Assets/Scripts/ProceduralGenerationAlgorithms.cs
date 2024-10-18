1e49c31a47ba32f7c9c4fd38e730ea5551af8ff1
dom lord <dominicrlord@gmail.com>
2024-10-19 00:48:37 +0100

    random walk

---
+++ Final year project/Assets/Scripts/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerationAlgorithms : MonoBehaviour
+{
+    /*
+     * Random walk algorithm
+     * HashSet stores a list of values of type <T>. A set is a collection of values that cannot contain duplicate elements.
+     */
+
+    public HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        var path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for (var i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
+        }
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionsList = new()
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left;
+    };
+
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+    }
+}
